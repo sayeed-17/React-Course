3a732822b9a642e575ab491db78da1eecdf73de0
sayeed-17 <syedsayeedtech@gmail.com>
2025-04-10 16:17:44 +0530

    episode 8 lifecycle methods of class components

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,51 @@
-import User from "./User";
+import React from "react";
 import UserClass from "./UserClass";
-const About = ()=>{
-    return(
-        <div>
-            <h1>This is Namaste React web series</h1>
-            {/* <User name={"sayeed(function)"}/> */}
-            <UserClass name={"sayeed(class)"} location={"Kakinada"}/>
-        </div>
-    )
+
+class About extends React.Component{
+    constructor(props){
+        super(props);
+        console.log("parent constructor called")
+    }
+    componentDidMount(){
+        console.log("parent component mounted")
+    }
+    render(){
+        console.log("parent render called")
+        return(
+            <div>
+                <h1>This is Namaste React web series</h1>
+                {/* <User name={"sayeed(function)"}/> */}
+                <UserClass name={"sayeed(class)"} location={"Kakinada"}/>
+                <UserClass name={"sameer"} location={"amaravati"}/>
+            </div>
+        )
+
+    }
 }
 export default About;
+
+//here u may think this as the hierarchy of componentDidMount
+// parent constructor called
+// parent render called
+// sayeed constructor called
+// sayeed render called
+// sayeed componentDidMount called
+// sameer constructor called
+// sameer render called
+// sameer componentDidMount called
+// parent componentDidMount called
+// //but this is false because....react has a small concept here which is called as reconciliation....
+//react class components lifecycle methods for componentDidMount has 2 phases 1.render phase and 2.commit phase
+//in render phase both the constructor and render of all the children are called and batched as one 
+//in commit phase the Actual DOM changes will be happening so instead of changing it again and agian the react will batch the constructor and render aso one and then it make the changes in DOM ..
+//Then the actaul hierarchy will be like this 
+// parent constructor called
+// parent render called
+// sayeed constructor called---render phase
+// sayeed render called---render phase
+// sameer constructor called---render phase
+// sameer render called---render phase
+{/* < DOM GETS UPDATED HERE IN SINGLE BATCH > */}
+// sayeed componentDidMount---commit phase
+// sameer componentDidMount---commit phase
+// parent componentDidMount---commit phase
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,22 +2,49 @@ import React from "react"
 class UserClass extends React.Component{
     constructor(props){
         super(props);
-        console.log(props);
+        
+        // this.state={
+        //     count:0,
+        // }
         this.state={
-            count:0,
+            userinfo:{
+                name:"dummy data",
+                location:"chennai",
+            }
         }
+        console.log(this.props.name,"constructor called")
+    }
+    async componentDidMount(){
+
+        console.log(this.props.name,"component mounted")
+        const data=await fetch("https://api.github.com/users/sayeed-17")
+        const json=await data.json();
+        console.log(json);
+        this.setState(
+            {
+                userinfo:json,
+            }
+        )
+    }
+    componentDidUpdate(){
+        console.log("component Did Update")
+    }
+    componentWillUnmount(){
+        console.log("component will unmount")
     }
     render(){
         // const {name,location}=this.props;
-        const{count}=this.state;
+        // const{count}=this.state;
+        console.log(this.props.name,"render called")
         return(
             <div className="user-card">
-                <h1>count :{count} </h1>
-                <button onClick={()=>{
+                {/* <h1>count :{count} </h1> */}
+                {/* <button onClick={()=>{
                     this.setState({
                         count : this.state.count+1
                 })
-                }}>increase count</button>
+                }}>increase count</button> */}
+                <img src={this.state.userinfo.avatar_url}/>
             <h2>Name : {this.props.name}</h2>
             <h2>Location :{this.props.location} </h2>
             <h2>Contact : syedsayeedtech@gmail.com</h2>
@@ -37,3 +64,31 @@ export default UserClass;
 //here to craete a state variable inside constructor u will be using this.state={......} like this in which this.state is a big object which consists of all the state variables where as in the functional components all the state variabkes will be defined in the different lines.
 
 //here to change the state in class components u make use of the this.setState() function which is returns a object so write the logic in curly braces.do not try to change the state variables directly in both classs and functional components...make use of the setCount(variable name)and this.setState in class components...dont directly overwrite like this.state.count=this.state.count+1; yeh galat haiii
+
+
+
+
+//LIFE CYCLE METHODS IN CLASS COMPONENTS 
+//let us assume that we have a parent class component(About) as well as a child class component(UserClass).....while executing first constructor is called and then render is called of About and then if in parent class component it has the child class component <UserClass/> in this case....it will call the constructor an render  of UserClass and then it will call the componentDidMount() function because constructor and render is called and then component gets loaded and once it gets loaded then parent componet gets loaded......
+
+// componentDidMount() is similar to that of useEffect in functional components and we will call teh apis only when the component gets loaded right??????? so first we will confirm that component gets loaded and then we will be calling the api calls inside it.
+
+
+//This is how the api call is done(only for single instancenot for multiple)
+//----------MOUNTING PROCESS----------------
+// constructor is called
+// state varaibles is set(dummy)
+// render is called
+// updates the dummy values for few milli seconds
+//componentDidMount is called
+//api is called
+
+
+//render is called once again
+//upadtes the state variables using this.setState() and renders it
+
+//componentDidUpdate is called after update gets complted
+//when u change the page if it is seen on screen that means component is mounted/remdered if that component is not seen on screen then it is said to be as component is  unmounted....so here whenever u change the screen from about to contact pahe componentwillunmount will be called.
+
+
+//CONSTRUCTOR--->RENDER---->COMPONENT DID MOUNT--->API CALL--->RENDER AGAIN(AS SETSTATE IS CALLED)-->component did upadte---->component will unmount
